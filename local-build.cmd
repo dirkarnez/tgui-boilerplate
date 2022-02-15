0b3da6e4d5d7259ddce59bab9c6e8bc08ec66994
dirkarnez <smalldirkalex@gmail.com>
2022-02-15 18:08:07 +0800

    - working

--- local-build.cmd
+++ local-build.cmd
@@ -3,7 +3,7 @@ REM run as Administrator
 cd /d %~dp0
 set DOWNLOAD_DIR=%USERPROFILE%\Downloads
 set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
-SET PATH=%DOWNLOAD_DIR%\PortableGit\bin;%DOWNLOAD_DIR%\mingw64-posix;%DOWNLOAD_DIR%\mingw64-posix\bin;%DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;%PATH%
+SET PATH=%DOWNLOAD_DIR%\PortableGit\bin;%DOWNLOAD_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;%DOWNLOAD_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;%DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;%PATH%
 
 cmake.exe -G"MinGW Makefiles" -DTGUI_ROOT="%DOWNLOAD_DIR_LINUX%/TGUI-0.9.3-mingw-7.3.0-64bit-for-SFML-2.5.1/TGUI-0.9/lib/cmake/TGUI" -DSFML_ROOT="%DOWNLOAD_DIR_LINUX%/sfml-v2.5.1-mingw/SFML/build/sfml/lib/cmake/SFML" -B./build
 cd build
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,45 @@
 
 int main()
 {
-    sf::RenderWindow window{{800, 600}, "TGUI window with SFML"};
+    sf::RenderWindow window{{800, 600}, "TGUI window with SFML", sf::Style::None}; // <- Important!! Style=None removes title
+
+    sf::Vector2i grabbedOffset;
+    bool grabbedWindow = false;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Escape)
+                    window.close();
+            }
+            else if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    grabbedOffset = window.getPosition() - sf::Mouse::getPosition();
+                    grabbedWindow = true;
+                }
+            }
+            else if (event.type == sf::Event::MouseButtonReleased)
+            {
+                if (event.mouseButton.button == sf::Mouse::Left)
+                    grabbedWindow = false;
+            }
+            else if (event.type == sf::Event::MouseMoved)
+            {
+                if (grabbedWindow)
+                    window.setPosition(sf::Mouse::getPosition() + grabbedOffset);
+            }
+        }
+        window.clear(sf::Color::Red);
+        window.display();
+    }
+
     tgui::GuiSFML gui{window};
     gui.mainLoop();
 }
