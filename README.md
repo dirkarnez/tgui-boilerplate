488222bcfbcdff061a111d913e8a6fd7d975e191
dirkarnez <smalldirkalex@gmail.com>
2022-02-15 17:47:15 +0800

    - init, wip

---
+++ README.md
@@ -0,0 +1,3 @@
+dotenv-cpp-boilerplate
+======================
+WIP. For Windows build, should use msvc - gcc does not work due to private `setenv` implementation in  [laserpants/dotenv-cpp](https://github.com/laserpants/dotenv-cpp)
---
+++ local-build.cmd
@@ -0,0 +1,14 @@
+REM run as Administrator
+@echo off
+cd /d %~dp0
+set DOWNLOAD_DIR=%USERPROFILE%\Downloads
+set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
+SET PATH=%DOWNLOAD_DIR%\PortableGit\bin;%DOWNLOAD_DIR%\mingw64-posix;%DOWNLOAD_DIR%\mingw64-posix\bin;%DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;%PATH%
+
+cmake.exe -G"MinGW Makefiles" -DTGUI_ROOT="%DOWNLOAD_DIR_LINUX%/TGUI-0.9.3-mingw-7.3.0-64bit-for-SFML-2.5.1/TGUI-0.9/lib/cmake/TGUI" -DSFML_ROOT="%DOWNLOAD_DIR_LINUX%/sfml-v2.5.1-mingw/SFML/build/sfml/lib/cmake/SFML" -B./build
+cd build
+
+:rebuild_and_startapp
+mingw32-make.exe
+pause
+REM GOTO rebuild_and_startapp
